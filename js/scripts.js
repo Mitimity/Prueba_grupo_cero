04c7a61ca6b135960cb2f70ab93cbcf875ba7b0a
Mitimity <mar.riosv@duocuc.cl>
2021-05-01 17:45:00 -0400

    01050544

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-function validarFormInsc(){
-    
+/*Validar formulario*/
+function validarFormInsc() {
+    validacionClave();
+}
+
+/*Validar contraseñas*/
+function validacionClave() {
+    var clave1 = document.getElementById('txtClave').value;
+    var clave2 = document.getElementById('txtClaveConf').value;
+    if (clave1 == clave2) {
+        return true;
+    }
+    else {
+        alert('Las contraseñas deben ser iguales.');
+        return false;
+    }
 }
